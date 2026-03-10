8dda58787a86cb2c7c5a613f4d968557fe9979f6
Riley Morrison <rileymo@uchicago.edu>
2026-03-10 13:59:32 -0500

    test webhook trigger

--- index.js
+++ index.js
@@ -64,4 +64,4 @@ index.post('/products', async (req, res) => {
 const PORT = process.env.PORT || 3001;
 index.listen(PORT, () => {
     console.log(`Product service running on port ${PORT}`);
-});
+});// webhook test
